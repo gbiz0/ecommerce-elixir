76427795432b52135208e84125623d146d811541
Ravi <ravi@example.com>
2025-11-18 09:48:22 -0300

    chore: remove bloat from router

--- commerce_server/lib/commerce_server_web/router.ex
+++ commerce_server/lib/commerce_server_web/router.ex
@@ -53,32 +53,4 @@ defmodule CommerceServerWeb.Router do
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
-
-  ## Authentication routes
-
-  scope "/", CommerceServerWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
-      on_mount: [{CommerceServerWeb.UserAuth, :require_authenticated}] do
-      live "/users/settings", UserLive.Settings, :edit
-      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
-    end
-
-    post "/users/update-password", UserSessionController, :update_password
-  end
-
-  scope "/", CommerceServerWeb do
-    pipe_through [:browser]
-
-    live_session :current_user,
-      on_mount: [{CommerceServerWeb.UserAuth, :mount_current_scope}] do
-      live "/users/register", UserLive.Registration, :new
-      live "/users/log-in", UserLive.Login, :new
-      live "/users/log-in/:token", UserLive.Confirmation, :new
-    end
-
-    post "/users/log-in", UserSessionController, :create
-    delete "/users/log-out", UserSessionController, :delete
-  end
 end
