406e7f4299b02889cc461aa727e0399289a0e169
grigolin <guigrigolin@gmail.com>
2025-11-25 08:07:28 -0300

    fix welcome

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,7 +9,7 @@ function HomePage() {
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold">Bem-vindo ao nosso E-Commerce</h1>
-      {user && <p className="mt-4 text-lg">Você está logado como {user.username}.</p>}
+      {user && <p className="mt-4 text-lg">Você está logado como {user.email}.</p>}
     </div>
   );
 }
